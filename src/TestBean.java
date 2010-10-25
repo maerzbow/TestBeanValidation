fb499d57a195b3045582d897a7cfa5829367861a
merzma <maerzbow@gmail.com>
2010-10-25 23:02:50 +0200

    nochn git test

--- src/TestBean.java
+++ src/TestBean.java
@@ -8,7 +8,9 @@ import org.hibernate.validator.constraints.Length;
 
 import at.solvistas.commons.validation.Violations;
 
+/** nochn GIT Tests **/
 public class TestBean {
+
 	@NotNull
 	private String notNull;
 	@Length(min = 3, max = 5)
@@ -16,14 +18,12 @@ public class TestBean {
 	@Future
 	private Date jetzt = new Date();
 
-	
-
 	public static void main(String[] args) {
 		TestBean t = new TestBean();
 
-		
-		at.solvistas.commons.validation.Validator validator = new at.solvistas.commons.validation.Validator(Locale.GERMAN);
-		
+		at.solvistas.commons.validation.Validator validator = new at.solvistas.commons.validation.Validator(
+				Locale.GERMAN);
+
 		Violations result = validator.validate(t);
 		System.out.println(result.toString());
 
