415a469c747897023ed41ced93915b11e05a33ca
maerzbow <maerzbow@gmail.com>
2010-10-25 14:07:50 -0700

    repo änderung

--- src/TestBean.java
+++ src/TestBean.java
@@ -8,7 +8,7 @@ import org.hibernate.validator.constraints.Length;
 
 import at.solvistas.commons.validation.Violations;
 
-/** nochn GIT Tests **/
+/** nochn GIT Tests im repo **/
 public class TestBean {
 
 	@NotNull
