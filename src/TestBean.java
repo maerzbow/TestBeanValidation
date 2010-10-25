2172e7362e67563d7de06045b50fc6e0b667fb94
maerzbow <maerzbow@gmail.com>
2010-10-25 14:43:38 -0700

    die 3 te remote

--- src/TestBean.java
+++ src/TestBean.java
@@ -8,7 +8,7 @@ import org.hibernate.validator.constraints.Length;
 
 import at.solvistas.commons.validation.Violations;
 
-/** nochn GIT Tests lokal die 2 te remote **/
+/** nochn GIT Tests lokal die 3 te remote **/
 public class TestBean {
 
 	@NotNull
