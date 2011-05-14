ac43adcd9e7bbbe7b53d2960fc7ce6f348be9ee7
maerzbow <maerzbow@gmail.com>
2011-05-14 22:12:50 +0200

    CleanCode says comments are bad!

--- src/TestBean.java
+++ src/TestBean.java
@@ -8,7 +8,6 @@ import org.hibernate.validator.constraints.Length;
 
 import at.solvistas.commons.validation.Violations;
 
-/** nochn GIT Tests lokal die 3 te remote **/
 public class TestBean {
 
 	@NotNull
